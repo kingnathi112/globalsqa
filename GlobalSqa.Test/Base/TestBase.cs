cb6be00647e5efded10de1faf0a8816ac0688413
NkosinathiM <NkosinathiM@slingshotstudios.com>
2021-11-10 21:23:43 +0200

    Created Testbase skeleton

---
+++ GlobalSqa.Test/Base/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace GlobalSqa.Test.Base
+{
+    public class TestBase
+    {
+        [OneTimeSetUp]
+        public void BeforeAllTests()
+        {
+
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+
+        }
+    }
+}
